ae02632026aff8b23a8965a59e1152ce497d7bf9
Steve Dunstan <steve_dunstan@yahoo.com>
2015-10-24 16:33:50 -0700

    Widget

---
+++ app/src/main/java/barqsoft/footballscores/ScoresWidgetConfigure.java
@@ -0,0 +1,12 @@
+package barqsoft.footballscores;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ScoresWidgetConfigure extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
